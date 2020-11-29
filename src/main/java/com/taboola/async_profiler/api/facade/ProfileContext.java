f636c0fb034456e84a72e91163bbc6fb0e6a6555
Tom Sisso <tom.s@taboola.com>
2020-11-29 09:02:35 +0200

    use lombok

--- src/main/java/com/taboola/async_profiler/api/facade/ProfileContext.java
+++ src/main/java/com/taboola/async_profiler/api/facade/ProfileContext.java
@@ -1,8 +1,10 @@
 package com.taboola.async_profiler.api.facade;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
+import lombok.Value;
+
+@Value
 public class ProfileContext {
     private final ProfileRequest profileRequest;
     private final LocalDateTime startTime;
@@ -17,47 +19,5 @@ public class ProfileContext {
         this.startTime = startTime;
         this.tmpFilePath = tmpFilePath;
         this.requestThread = requestThread;
-    }
-
-    public ProfileRequest getProfileRequest() {
-        return profileRequest;
-    }
-
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-
-    public String getTmpFilePath() {
-        return tmpFilePath;
-    }
-
-    public Thread getRequestThread() {
-        return requestThread;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ProfileContext that = (ProfileContext) o;
-        return Objects.equals(profileRequest, that.profileRequest) &&
-                Objects.equals(startTime, that.startTime) &&
-                Objects.equals(tmpFilePath, that.tmpFilePath) &&
-                Objects.equals(requestThread, that.requestThread);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(profileRequest, startTime, tmpFilePath, requestThread);
-    }
-
-    @Override
-    public String toString() {
-        return "ProfileContext{" +
-                "profileRequest=" + profileRequest +
-                ", startTime=" + startTime +
-                ", tmpFilePath='" + tmpFilePath + '\'' +
-                ", requestThread=" + requestThread +
-                '}';
     }
 }
--- src/main/java/com/taboola/async_profiler/api/facade/ProfileRequest.java
+++ src/main/java/com/taboola/async_profiler/api/facade/ProfileRequest.java
@@ -1,9 +1,12 @@
 package com.taboola.async_profiler.api.facade;
 
+import lombok.Data;
+
 import java.util.Objects;
 
 import com.taboola.async_profiler.api.original.Events;
 
+@Data
 public class ProfileRequest {
 
     private int durationSeconds = 60;//profiling duration
@@ -16,122 +19,11 @@ public class ProfileRequest {
     private String includedTraces;
     private String excludedTraces;
 
-    public ProfileRequest() {
-    }
-
-    public int getDurationSeconds() {
-        return durationSeconds;
-    }
-
-    public void setDurationSeconds(int durationSeconds) {
-        this.durationSeconds = durationSeconds;
-    }
-
-    public Integer getSamplingIntervalMs() {
-        return samplingIntervalMs;
-    }
-
-    public void setSamplingIntervalMs(Integer samplingIntervalMs) {
-        this.samplingIntervalMs = samplingIntervalMs;
-    }
-
-    public Integer getSamplingIntervalBytes() {
-        return samplingIntervalBytes;
-    }
-
-    public void setSamplingIntervalBytes(Integer samplingIntervalBytes) {
-        this.samplingIntervalBytes = samplingIntervalBytes;
-    }
-
-    public int getFrameBufferSize() {
-        return frameBufferSize;
-    }
-
-    public void setFrameBufferSize(int frameBufferSize) {
-        this.frameBufferSize = frameBufferSize;
-    }
-
-    public String getEventType() {
-        return eventType;
-    }
-
-    public void setEventType(String eventType) {
-        this.eventType = eventType;
-    }
-
-    public String getIncludedThreads() {
-        return includedThreads;
-    }
-
-    public void setIncludedThreads(String includedThreads) {
-        this.includedThreads = includedThreads;
-    }
-
-    public String getIncludedTraces() {
-        return includedTraces;
-    }
-
-    public void setIncludedTraces(String includedTraces) {
-        this.includedTraces = includedTraces;
-    }
-
-    public String getExcludedTraces() {
-        return excludedTraces;
-    }
-
-    public void setExcludedTraces(String excludedTraces) {
-        this.excludedTraces = excludedTraces;
-    }
-
-    public String getFormat() {
-        return format;
-    }
-
-    public void setFormat(String format) {
-        this.format = format;
-    }
-
     public boolean hasIncludedThreads() {
         return includedThreads != null && !includedThreads.equals("");
     }
 
     public boolean isFlameGraphRequest() {
         return "svg".equals(format) || "flamegraph".equals(format);
-    }
-
-    @Override
-    public String toString() {
-        return "ProfileRequest{" +
-                "durationSeconds=" + durationSeconds +
-                ", frameBufferSize=" + frameBufferSize +
-                ", samplingIntervalMs=" + samplingIntervalMs +
-                ", samplingIntervalBytes=" + samplingIntervalBytes +
-                ", eventType='" + eventType + '\'' +
-                ", format='" + format + '\'' +
-                ", includedThreads='" + includedThreads + '\'' +
-                ", includedTraces='" + includedTraces + '\'' +
-                ", excludedTraces='" + excludedTraces + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ProfileRequest that = (ProfileRequest) o;
-        return durationSeconds == that.durationSeconds &&
-                frameBufferSize == that.frameBufferSize &&
-                Objects.equals(samplingIntervalMs, that.samplingIntervalMs) &&
-                Objects.equals(samplingIntervalBytes, that.samplingIntervalBytes) &&
-                Objects.equals(eventType, that.eventType) &&
-                Objects.equals(format, that.format) &&
-                Objects.equals(includedThreads, that.includedThreads) &&
-                Objects.equals(includedTraces, that.includedTraces) &&
-                Objects.equals(excludedTraces, that.excludedTraces);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(durationSeconds, frameBufferSize, samplingIntervalMs, samplingIntervalBytes, eventType, format, includedThreads, includedTraces, excludedTraces);
     }
 }
